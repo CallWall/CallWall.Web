931b248b4f35e0341ccc99a4104ca48a74e277aa
Lee Campbell <Lee.ryan.campbell@gmail.com>
2013-08-09 23:17:02 +0100

    Add nav link tracking

---
+++ src/CallWall.Web/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace CallWall.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        public ActionResult Manage()
+        {
+            return View();
+        }
+
+
+        public ActionResult LogOff()
+        {
+            return new RedirectResult("/");
+        }
+    }
+}
