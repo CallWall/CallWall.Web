5161279af7915bf6fa9a712ef63a3ea13eeda23a
Lee Campbell <Lee.ryan.campbell@gmail.com>
2013-09-12 10:24:57 +0100

    Moving Google Provider to seperate assembly, loading dynamically

--- src/CallWall.Web/Bootstrapper.cs
+++ src/CallWall.Web/Bootstrapper.cs
@@ -1,7 +1,10 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Web.Mvc;
 using CallWall.Web.Hubs;
 using CallWall.Web.Providers;
-using CallWall.Web.Providers.Google;
 using CallWall.Web.Unity;
 using Microsoft.Practices.Unity;
 using Unity.Mvc4;
@@ -17,6 +20,26 @@ namespace CallWall.Web
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
 
             return container;
+        }
+    }
+
+    public sealed class TypeRegistry : ITypeRegistry
+    {
+        private readonly IUnityContainer _container;
+
+        public TypeRegistry(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public void RegisterType<TFrom, TTo>() where TTo : TFrom
+        {
+            _container.RegisterType<TFrom, TTo>();
+        }
+
+        public void RegisterType<TFrom, TTo>(string name) where TTo : TFrom
+        {
+            _container.RegisterType<TFrom, TTo>(name);
         }
     }
 
@@ -40,13 +63,44 @@ namespace CallWall.Web
         public static void RegisterTypes(IUnityContainer container)
         {
             container.RegisterType<Providers.ISecurityProvider, Providers.SecurityProvider>();
-
-            //GOOGLE
-            container.RegisterType<IContactsProvider, GoogleContactsProvider>();
-            container.RegisterType<IAccountAuthentication, GoogleAuthentication>("GoogleAuthentication");
-
-
             container.RegisterType<ContactsHub>(new InjectionFactory(CreateContactsHub));
+
+            InitialiseModules(container);
+        }
+
+        private static void InitialiseModules(IUnityContainer container)
+        {
+            var typeRegistry = new TypeRegistry(container);
+
+            var providersPath = GetProvidersPath();
+
+            //Look for implementations of IModule in each assembly from providers folder
+            var modules = from file in Directory.EnumerateFiles(providersPath)
+                    where file.EndsWith(".dll")
+                    let assembly = Assembly.LoadFile(file)
+                    from type in assembly.GetTypes()
+                    where IsModule(type)
+                              select (IModule)Activator.CreateInstance(type);
+                    
+            foreach (var module in modules)
+            {
+                module.Initialise(typeRegistry);
+            }
+        }
+
+        private static string GetProvidersPath()
+        {
+            var webRoot = System.AppDomain.CurrentDomain.BaseDirectory;
+            var providersPath = Path.Combine(webRoot, @"bin\Providers");
+            return providersPath;
+        }
+
+        public static bool IsModule(Type type)
+        {
+            var moduleType = typeof (IModule);
+            return type.IsPublic
+                   && !type.IsAbstract
+                   && moduleType.IsAssignableFrom(type);
         }
 
         private static object CreateContactsHub(IUnityContainer arg)
