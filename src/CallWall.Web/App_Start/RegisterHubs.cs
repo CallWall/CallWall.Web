3ee47f037f9380b09cb9422b52732d46a19750fb
Lee Campbell <Lee.ryan.campbell@gmail.com>
2013-09-28 15:15:56 +0100

    Cleaning up logging

--- src/CallWall.Web/App_Start/RegisterHubs.cs
+++ src/CallWall.Web/App_Start/RegisterHubs.cs
@@ -2,11 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Routing;
-using CallWall.Web;
 using Microsoft.AspNet.SignalR;
 using Microsoft.Practices.Unity;
-
-//[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(RegisterHubs), "Start")]
 
 namespace CallWall.Web
 {
@@ -15,8 +12,6 @@ namespace CallWall.Web
         public static void Start(IUnityContainer container)
         {
             // Register the default hubs route: ~/signalr
-            //var container = Container.Create();
-
             var config = new HubConfiguration
                 {
                     Resolver = new UnitySignalRDependencyResolver(container)
--- src/CallWall.Web/Global.asax.cs
+++ src/CallWall.Web/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
+using CallWall.Web.Logging;
 using CallWall.Web.Providers;
 using Microsoft.Practices.Unity;
 
@@ -13,32 +14,53 @@ namespace CallWall.Web
 
     public class MvcApplication : System.Web.HttpApplication
     {
+        private readonly ILogger _logger;
         private IUnityContainer _container;
+
+        public MvcApplication()
+        {
+            _logger =new Log4NetLogger(GetType());
+        }
 
         protected void Application_Start()
         {
+            _logger.Info("Starting Application...");
             _container = Bootstrapper.Initialise();
             RegisterHubs.Start(_container);
-
             AreaRegistration.RegisterAllAreas();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            
+            _logger.Info("Application started.");
         }
 
         protected void FormsAuthentication_OnAuthentication(object sender, FormsAuthenticationEventArgs args)
         {
+            _logger.Info("Authentication user...");
             //HACK: How do I inject the security provider into the global asax?
             //Perhaps this : http://www.hanselman.com/blog/IPrincipalUserModelBinderInASPNETMVCForEasierTesting.aspx 
             var securityProvider = _container.Resolve<ISecurityProvider>();
 
             var principal = securityProvider.GetPrincipal(args.Context.Request);
             if (principal != null)
+            {
                 args.Context.User = principal;
+                _logger.Info("User authenticated.");
+            }
+            else
+            {
+                _logger.Info("User is anonymous.");
+            }
+        }
+
+        public override void Dispose()
+        {
+            _logger.Info("Application being disposed.");
+            using (_container)
+            {}
+
+            base.Dispose();
         }
     }
 }
