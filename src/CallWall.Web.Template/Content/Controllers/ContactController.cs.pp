354171b2a16c1eaf3d067b5dee95b8c443c3f2df
Lee Campbell <Lee.ryan.campbell@gmail.com>
2013-11-24 22:00:49 +0000

    Adding the Web host template package source

---
+++ src/CallWall.Web.Template/Content/Controllers/ContactController.cs.pp
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace $rootnamespace$.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ActionResult Detail(string identifier)
+        {
+            return View();
+        }
+    }
+}
