b75663b587ebb38514d7431961130b4cd0596549
Lee Campbell <Lee.ryan.campbell@gmail.com>
2013-12-06 09:30:51 +0000

    Moved security implementations from Web to Web.Core

--- src/CallWall.Web.Core/ISession.cs
+++ src/CallWall.Web.Core/ISession.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace CallWall.Web
 {
+    //TODO: Should this also have an Account property? eg. so we can differentiate between accounts from the same provider?
+    //  i.e. Lee@gmail.com & Lee_work@gmail.com
+    //TODO: Add username to this account property type too. -LC
     public interface ISession
     {
         string Provider { get; }
