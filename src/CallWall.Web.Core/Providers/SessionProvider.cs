16936ed5081fc25eaafbd0eb8c5925a585bd32f3
Lee Campbell <Lee.ryan.campbell@gmail.com>
2013-12-09 14:41:24 +0000

    Correcting merge

--- src/CallWall.Web.Core/Providers/SessionProvider.cs
+++ src/CallWall.Web.Core/Providers/SessionProvider.cs
@@ -5,6 +5,8 @@ using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace CallWall.Web.Providers
 {
@@ -32,8 +34,11 @@ namespace CallWall.Web.Providers
 
         public void SetPrincipal(Controller controller, ISession session)
         {
-            var state = session.Serialize();
-            var authTicket = new FormsAuthenticationTicket(1, session.AccountDetails.DisplayName, DateTime.UtcNow, DateTime.MaxValue, true, state, "CallWallAuth");
+            var sessions = GetSessions(controller.Request.Cookies).ToDictionary(x => x.Provider);
+            sessions.Add(session.Provider, session);
+            var jObject = JObject.FromObject(sessions);
+            var json = jObject.ToString(Formatting.None);
+            var authTicket = new FormsAuthenticationTicket(1, session.AccountDetails.DisplayName, DateTime.UtcNow, DateTime.MaxValue, true, json, "CallWallAuth");
             var encTicket = FormsAuthentication.Encrypt(authTicket);
             var faCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encTicket);
             controller.Response.Cookies.Add(faCookie);
@@ -42,6 +47,21 @@ namespace CallWall.Web.Providers
         public void LogOff()
         {
             FormsAuthentication.SignOut();
+        }
+        
+        private IEnumerable<ISession> GetSessions(HttpCookieCollection cookies)
+        {
+            if (!FormsAuthentication.CookiesSupported) return null;
+            var authCookie = cookies[FormsAuthentication.FormsCookieName];
+
+            if (authCookie != null)
+            {
+                var authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+                return authTicket == null ?
+                    Enumerable.Empty<ISession>()
+                    : GetSessions(authTicket);
+            }
+            return Enumerable.Empty<ISession>();
         }
 
         private IEnumerable<ISession> GetSessions(FormsAuthenticationTicket ticket)
