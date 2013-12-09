67f5fd73f4f564df594e0bda2b982786caa5b717
Lee Campbell <Lee.ryan.campbell@gmail.com>
2013-12-09 22:14:07 +0000

    Adding the OAuthAccountListItem template

---
+++ src/CallWall.Web.Template/Content/Model/OAuthAccountListItem.cs.pp
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using CallWall.Web;
+
+namespace $rootnamespace$.Models
+{
+    public class OAuthAccountListItem 
+    {
+        public OAuthAccountListItem(IAccountConfiguration accountConfiguration, bool isActive)
+        {
+            Name = accountConfiguration.Name;
+            Image = accountConfiguration.Image;
+            Resources = accountConfiguration.Resources;
+            IsActive = isActive;
+        }
+
+        public string Name { get; private set; }
+        public Uri Image { get; private set; }
+        public IEnumerable<IResourceScope> Resources { get; private set; }
+        public bool IsActive { get; private set; }
+    }
+}
