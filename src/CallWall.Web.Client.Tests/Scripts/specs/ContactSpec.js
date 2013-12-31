de93b4536353c2f18b42e1c9b66a68351a178586
RhysC <rhysevancampbell@gmail.com>
2013-12-31 18:48:19 +0800

    Contact View Model filter tests

--- src/CallWall.Web.Client.Tests/Scripts/specs/ContactSpec.js
+++ src/CallWall.Web.Client.Tests/Scripts/specs/ContactSpec.js
@@ -3,16 +3,39 @@
 /// <reference path="../jasmine/jasmine.js" />
 
 describe("Contacts", function () {
-  
-  it("should be able to create ContactViewModel", function () {
-      var contact = { Title : 'abc', Tags : ['alpha', 'beta']};
-      var contactViewModel = new ContactViewModel(contact);
-      console.log(contactViewModel);
-      expect(contactViewModel).toBeDefined();
-      expect(contactViewModel.titleUpperCase).toBe('ABC');
-      expect(contactViewModel.primaryAvatar).toBe('/Content/images/AnonContact.svg');
-      expect(contactViewModel.isVisible).toBeTruthy();
-      expect(contactViewModel.tags).toEqual(contact.Tags);
+    var anoncontactSvg = '/Content/images/AnonContact.svg';
 
-  });
+    describe("ContactViewModel", function () {
+        var contact, contactViewModel;
+
+        beforeEach(function () {
+            contact = { Title: 'abc', Tags: ['alpha', 'beta'] };
+            contactViewModel = new ContactViewModel(contact);
+        });
+
+        it("should be able to created with a valid contact", function () {
+            expect(contactViewModel).toBeDefined();
+            expect(contactViewModel.titleUpperCase).toBe('ABC');
+            expect(contactViewModel.primaryAvatar).toBe(anoncontactSvg);
+            expect(contactViewModel.isVisible()).toBeTruthy();
+            expect(contactViewModel.tags).toEqual(contact.Tags);
+        });
+        
+        describe("filter", function () {
+            it("should be visible when filter text is start of title", function () {
+                var validFilters = ['A', 'AB', 'ABC'];
+                validFilters.forEach(function(prefixText) {
+                    contactViewModel.filter(prefixText);
+                    expect(contactViewModel.isVisible()).toBeTruthy();
+                });
+            });
+            it("should not be visible when filter text is not start of title", function () {
+                var invalidFilters = ['ABCD', 'B', ' '];
+                invalidFilters.forEach(function (prefixText) {
+                    contactViewModel.filter(prefixText);
+                    expect(contactViewModel.isVisible()).toBeFalsy();
+                });
+            });
+        });
+    });
 });
