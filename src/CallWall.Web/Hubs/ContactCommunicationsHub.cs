a2895116ef512309b409a5a1d1355e7db700748d
Lee Campbell <Lee.ryan.campbell@gmail.com>
2014-02-08 23:37:05 +0000

    Async Rx instead of blocking Thread.Sleep

--- src/CallWall.Web/Hubs/ContactCommunicationsHub.cs
+++ src/CallWall.Web/Hubs/ContactCommunicationsHub.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reactive.Concurrency;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using System.Threading;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
 
@@ -22,19 +20,20 @@ namespace CallWall.Web.Hubs
 
         public void Subscribe(string[] contactKeys)
         {
+            //TODO: Replace with Rx Logger --https://gist.github.com/LeeCampbell/3817281
             try
             {
                 _logger.Debug("RequestContactProfile({0})", string.Join(",", contactKeys));
-                var subscription = GetMessages()
-                .ToObservable(Scheduler.ThreadPool)
-                .Subscribe(
-                    message => Clients.Caller.OnNext(message),
-                    ex =>
-                    {
-                        _logger.Error(ex, "Error in getting Messages");
-                        Clients.Caller.OnError("Unable to get communications");
-                    },
-                    () => Clients.Caller.OnCompleted());
+                var subscription =  Observable.Interval(TimeSpan.FromSeconds(2))
+                    .Zip(GetMessages(), (_, msg) => msg)
+                    .Subscribe(
+                        message => Clients.Caller.OnNext(message),
+                        ex =>
+                        {
+                            _logger.Error(ex, "Error in getting Messages");
+                            Clients.Caller.OnError("Unable to get communications");
+                        },
+                        () => Clients.Caller.OnCompleted());
                 _contactComunicationSubscription.Disposable = subscription;
             }
             catch (Exception ex)
@@ -47,11 +46,11 @@ namespace CallWall.Web.Hubs
         private static IEnumerable<Message> GetMessages()
         {
             var n = DateTime.Now;
-            yield return new Message(n.AddMinutes(-10), false, "On my way", null, "hangouts"); Thread.Sleep(TimeSpan.FromSeconds(2));
-            yield return new Message(n.AddMinutes(-13), true, "Dude, where are you?", null, "hangouts"); Thread.Sleep(TimeSpan.FromSeconds(2));
-            yield return new Message(n.AddDays(-2), false, "Pricing a cross example","Here is the sample we were talking about the other day. It should cover the basic case, the complex multi-leg option case and all the variations in-between. If you have any questions, then just email me back on my home account.","linkedin"); Thread.Sleep(TimeSpan.FromSeconds(2));
-            yield return new Message(n.AddDays(-4), false, "I will bring the food for the Rugby","From: James Alex To: You, Lee FAKE Camplell, Simon Real, Brian Baxter, Josh Taylor and Sally Hubbard","gmail"); Thread.Sleep(TimeSpan.FromSeconds(2));
-            yield return new Message(n.AddDays(-4), false, "CallWall are recruiting engineers now!", "Retweets : 7","twitter"); Thread.Sleep(TimeSpan.FromSeconds(2));
+            yield return new Message(n.AddMinutes(-10), false, "On my way", null, "hangouts");
+            yield return new Message(n.AddMinutes(-13), true, "Dude, where are you?", null, "hangouts");
+            yield return new Message(n.AddDays(-2), false, "Pricing a cross example","Here is the sample we were talking about the other day. It should cover the basic case, the complex multi-leg option case and all the variations in-between. If you have any questions, then just email me back on my home account.","linkedin");
+            yield return new Message(n.AddDays(-4), false, "I will bring the food for the Rugby","From: James Alex To: You, Lee FAKE Camplell, Simon Real, Brian Baxter, Josh Taylor and Sally Hubbard","gmail");
+            yield return new Message(n.AddDays(-4), false, "CallWall are recruiting engineers now!", "Retweets : 7","twitter");
             yield return new Message(n.AddDays(-5), true, "Rugby at my place on Saturday morning","To: James Alex, Simon Real + 3 others", "gmail");
         }
     }
