bed710d43145fefc431d37e824ec52c35f40fa27
Lee Campbell <lee.ryan.campbell@gmail.com>
2015-02-11 00:27:41 +0000

    Upgrade EventStore to 3.0. Can run in local path now

--- src/CallWall.Web.EventStore.Tests/Doubles/InMemoryEventStoreConnectionFactory.cs
+++ src/CallWall.Web.EventStore.Tests/Doubles/InMemoryEventStoreConnectionFactory.cs
@@ -12,17 +12,17 @@ namespace CallWall.Web.EventStore.Tests.Doubles
         private readonly IPEndPoint _ipEndPoint;
         private readonly Process _eventStoreProcess;
         
-        //TODO: When the EventStore Chocolatey package is corrected, then run from ~\Server\EventStore\EventStore.SingleNode.exe
-        public InMemoryEventStoreConnectionFactory() : this(@"C:\Program Files\eventstore\EventStore-NET-v3.0.0rc2\EventStore.SingleNode.exe","127.0.0.1", 1113)
+        public InMemoryEventStoreConnectionFactory()
+            : this(@"..\..\..\..\Server\EventStore\EventStore.ClusterNode.exe", "127.0.0.1", 1113)
         {}
         public InMemoryEventStoreConnectionFactory(string eventStorePath, string ipAddress, int port)
         {
-            _eventStoreProcess = Process.Start(eventStorePath, "--mem-db --run-projections=ALL");
+            _eventStoreProcess = Process.Start(eventStorePath, "--mem-db --skip-db-verify --run-projections=ALL");
             var ip = IPAddress.Parse(ipAddress);
             _ipEndPoint = new IPEndPoint(ip, port);
             
             Trace.Write("Waiting for process to warm up..");
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < 3; i++)
             {
                 Thread.Sleep(1000);
                 Trace.Write(".");
--- tools/InstallEventStoreServer.bat
+++
@@ -1,8 +0,0 @@
-REM Install Chocolatey
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-
-
-REM Install EventStore to local directory (getEventStore.com)
-REM if not exist ..\server\EventStore\ MD ..\server\EventStore\
-REM choco install eventstore -version 3.0.0.3 -installArgs '..\server\EventStore\'
-choco install eventstore -version 3.0.0.3 -installArgs 'C:\Users\Lee\Documents\GitHub\CallWall.Web\server\EventStore'
