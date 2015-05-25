586c567f19653aab25f845aacc5c0cfb0eba570a
Lee Campbell <lee.ryan.campbell@gmail.com>
2015-05-25 11:25:06 +0100

    Normalizing Gmail addresses

--- src/CallWall.Web.Core/Domain/ContactEmailAddress.cs
+++ src/CallWall.Web.Core/Domain/ContactEmailAddress.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace CallWall.Web.Domain
 {
     public sealed class ContactEmailAddress : ContactHandle
@@ -11,8 +13,24 @@ namespace CallWall.Web.Domain
 
         public override string[] NormalizedHandle()
         {
-            //TODO: add gmail normalization rules
-            return new[] { Handle.ToLowerInvariant() };
+            var lcase = Handle.ToLowerInvariant();
+            return GmailHandleNormalizer.Normalize(lcase);
         }        
+    }
+
+    sealed class GmailHandleNormalizer
+    {
+        private static readonly Regex DomainRegex = new Regex(@"googlemail\.com", RegexOptions.Compiled);
+        private static readonly Regex AliasRegex = new Regex(@"(?=.*)\+.*(?=@gmail.com)", RegexOptions.Compiled);
+        private static readonly Regex LocalPartRegex = new Regex(@".*(?=@gmail.com)", RegexOptions.Compiled);
+        //assumes handle is lower case already.
+        public static string[] Normalize(string handle)
+        {
+            var correction = DomainRegex.Replace(handle, "gmail.com");
+            correction = AliasRegex.Replace(correction, string.Empty);
+            correction = LocalPartRegex.Replace(correction, match=>match.Value.Replace(".", string.Empty));
+
+            return new[] {correction};
+        }
     }
 }
